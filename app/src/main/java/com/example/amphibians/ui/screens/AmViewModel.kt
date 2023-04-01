3c66ea8fefbbd2c3c679d2dfc1997124f6091d12
Karuchiu <karuchiu8@gmail.com>
2023-04-01 22:08:27 +0300

    Added App container

--- app/src/main/java/com/example/amphibians/ui/screens/AmViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/AmViewModel.kt
@@ -4,7 +4,17 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.amphibians.AmApplication
+import com.example.amphibians.data.AmRepository
 import com.example.amphibians.model.AmphibianArticle
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed interface AmUiState{
     data class Success(val articles: List<AmphibianArticle>): AmUiState
@@ -12,7 +22,35 @@ sealed interface AmUiState{
     object Error: AmUiState
 }
 
-class AmViewModel: ViewModel() {
+class AmViewModel(
+    private val amRepository: AmRepository
+): ViewModel() {
     var amUiState: AmUiState by mutableStateOf(AmUiState.Loading)
         private set
+
+    init {
+        getAmArticles()
+    }
+
+    private fun getAmArticles(){
+        viewModelScope.launch {
+            amUiState = try {
+                AmUiState.Success(amRepository.getAmArticles())
+            }catch (e: IOException){
+                AmUiState.Error
+            }catch (e: HttpException){
+                AmUiState.Error
+            }
+        }
+    }
+
+    companion object{
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val application = (this[APPLICATION_KEY] as AmApplication)
+                val amRepository = application.container.amRepository
+                AmViewModel(amRepository = amRepository)
+            }
+        }
+    }
 }
